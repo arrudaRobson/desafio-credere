2147bbb53ca75596ab9f7422220b8b342a7afe7f
Robson Arruda <robson.arruda@uni9.edu.br>
2018-12-25 23:52:01 -0200

    Adds rspec and changes route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root "base#index"
   namespace :v1, defaults: { format: :json } do
-    get 'probe/position/:id', to: 'probe#position'
+    get 'probe/:id/position', to: 'probe#position'
+    put 'probe/:id/move', to: 'probe#move'
+    post 'probe', to: 'probe#create'
+    put 'probe/:id/position/reset', to: 'probe#reset'
   end
 end
--- db/migrate/20181224143835_create_probes.rb
+++ db/migrate/20181224143835_create_probes.rb
@@ -1,8 +1,8 @@
 class CreateProbes < ActiveRecord::Migration[5.2]
   def change
     create_table :probes do |t|
-      t.integer :x, default: 0
-      t.integer :y, default: 0
+      t.integer :xAxis, default: 0
+      t.integer :yAxis, default: 0
       t.string :face, default: 'D'
 
       t.timestamps
