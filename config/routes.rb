779435e24e74b768c52ef6904c76a8a892534713
Robson Arruda <robson.arruda@uni9.edu.br>
2019-01-03 06:28:07 -0200

    Fix error tratament

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'application#route_not_found'
-  match '*unmatched', to: 'application#route_not_found', via: :all
   namespace :v1, defaults: { format: :json } do
     get 'probe/:id/position', to: 'probe#position'
     patch 'probe/:id/movement', to: 'probe#move'
     post 'probe', to: 'probe#create'
     put 'probe/:id/position/initial', to: 'probe#reset'
   end
+  match '*unmatched', to: 'application#route_not_found', via: :all
 end
